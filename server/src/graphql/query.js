2f4cbe281feded0be7bf74079543719e168baa7b
Claudio4 <me@claudio4.com>
2017-09-23 02:49:54 +0100

    Ghabge folder structure for GraphQL

---
+++ server/src/graphql/query.js
@@ -0,0 +1,52 @@
+import {
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean
+} from 'graphql';
+
+import db from '../models';
+
+import User from './types/user';
+import Image from './types/image';
+
+export default new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    users: {
+      type: new GraphQLList(User),
+      args: {
+        id: {
+          type: GraphQLInt
+        },
+        email: {
+          type: GraphQLString
+        },
+        apiKey: {
+          type: GraphQLString
+        },
+        isAdmin: {
+          type: GraphQLBoolean
+        }
+      },
+      resolve(root, args) {
+        return db.User.findAll({ where: args });
+      }
+    },
+    images: {
+      type: new GraphQLList(Image),
+      args: {
+        id: {
+          type: GraphQLInt
+        },
+        publicId: {
+          type: GraphQLString
+        }
+      },
+      resolve(root, args) {
+        return db.Image.findAll({ where: args });
+      }
+    }
+  })
+})
--- server/src/graphql/schema.js
+++ server/src/graphql/schema.js
@@ -1,56 +1,6 @@
-import {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLList,
-  GraphQLInt,
-  GraphQLString,
-  GraphQLBoolean
-} from 'graphql';
+import {GraphQLSchema} from 'graphql';
 
-import db from '../models';
-
-import User from './user';
-import Image from './image';
-
-const Query = new GraphQLObjectType({
-  name: 'Query',
-  fields: () => ({
-    users: { 
-      type: new GraphQLList(User),
-      args: {
-        id: {
-          type: GraphQLInt
-        },
-        email: {
-          type: GraphQLString
-        },
-        apiKey: {
-          type: GraphQLString
-        },
-        isAdmin: {
-          type: GraphQLBoolean
-        }
-      },
-      resolve(root, args) {
-        return db.User.findAll({where: args});
-      }
-    },
-    images: {
-      type: new GraphQLList(Image),
-      args: {
-        id: {
-          type: GraphQLInt
-        },
-        publicId: {
-          type: GraphQLString
-        }
-      },
-      resolve(root, args) {
-        return db.Image.findAll({where: args});
-      }
-    }
-})
-})
+import Query from './query';
 
 export default new GraphQLSchema({
   query: Query
