be6f984574a443cb93d79fb86c425a9b97c5329e
Claudio4 <me@claudio4.com>
2017-09-24 04:19:45 +0100

    Refactor to development fuzzy data

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -3,7 +3,6 @@
 import fs        from 'fs';
 import path      from 'path';
 import Sequelize from 'sequelize';
-import Faker from 'faker';
 import nanoid from 'nanoid';
 import config    from '../utils/config';
 
@@ -33,20 +32,26 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 if (isDevelopment) {
-  sequelize.sync({force: true}).then(()=> {
-    for(let i = 0; i < 10; i++ ) {
-      db.User.create({
-        id: nanoid(10),
-        name: Faker.name.firstName(),
-        email: Faker.internet.email(),
-        apiKey: nanoid(24),
-        isAdmin: true
-      }).then(user => user.createImage({
-        id: nanoid(10),
-        extension: "jpg"
-      }));
-    }
-  })
+  var Faker = require('faker');
+  var bcrypt = require('bcrypt');
+  (async function() {
+    let passHash = await bcrypt.hash("1234", 12);
+    sequelize.sync({force: true}).then(()=> {
+      for(let i = 0; i < 10; i++ ) {
+        db.User.create({
+          id: nanoid(10),
+          name: Faker.name.firstName(),
+          password: passHash,
+          email: Faker.internet.email(),
+          apiKey: nanoid(24),
+          isAdmin: true
+        }).then(user => user.createImage({
+          id: nanoid(10),
+          extension: "jpg"
+        }));
+      }
+    })
+  })();
 }
 
 export default db;
