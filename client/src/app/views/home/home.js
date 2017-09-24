bb3eaccf73053fa5387f2dcf1ed958f5b63a3777
Guillem Arias <gariasfa7@alumnes.ub.edu>
2017-09-24 22:13:11 +0200

    Each color a variable

--- client/src/app/views/home/home.js
+++ client/src/app/views/home/home.js
@@ -1,4 +1,5 @@
 import { h, Component } from 'preact';
+import { Link } from 'preact-router/match';
 
 import Button from '../shared/button/button.js';
 import Logo from '../shared/logo/logo.js';
@@ -9,17 +10,17 @@ import style from './home.scss';
 export default class Home extends Component {
     render() {
         return (
-            <div class={`${style.Home} flex flex-full-center flex-dc`}>
+            <div class={`${style.home} flex flex-full-center flex-dc`}>
                 <div class="flex flex-cross-center flex-dc">
                     <Logo />
-                    <p class={`${style.HomeText} ta-c`}>This domain is using Nethloader, a <br /> self hosted media sharing service.</p>
+                    <p class={`${style.homeText} ta-c`}>This domain is using Nethloader, a <br /> self hosted media sharing service.</p>
                 </div>
-                <div class={`${style.HomeButtons} flex flex-full-center flex-sa`}>
+                <div class={`${style.homeButtons} flex flex-full-center flex-sa`}>
                     <a href="" rel="noopener" target="_blank"><Button big text="More info"/></a>
                     <a href="https://github.com/nethruster/nethloader" rel="noopener" target="_blank"><Button big transparent text="Github"/></a>
                 </div>
-                <div class={`${style.HomeLinks} flex flex-full-center`}>
-                    <p>Version 0.1.0&nbsp;&nbsp;&nbsp;&nbsp;<Icon iconName="twitter" />&nbsp;<a href="https://twitter.com/nethruster" rel="noopener" target="_blank">@nethruster</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="">Login</a></p>
+                <div class={`${style.homeLinks}`}>
+                    <p class="flex flex-full-center">Version 0.1.0&nbsp;&nbsp;&nbsp;&nbsp;<Icon iconName="twitter" />&nbsp;<a href="https://twitter.com/nethruster" rel="noopener" target="_blank">@nethruster</a>&nbsp;&nbsp;&nbsp;&nbsp;<Link href="/login">Login</Link></p>
                 </div>
             </div>
         );
