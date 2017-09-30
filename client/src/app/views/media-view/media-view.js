5d1a6ecdaec0a2ebd839d3fcae94e5e235a9c6bf
Guillem Arias <gariasfa7@alumnes.ub.edu>
2017-09-30 02:45:31 +0200

    Add basic header-nav

--- client/src/app/views/media-view/media-view.js
+++ client/src/app/views/media-view/media-view.js
@@ -1,11 +1,14 @@
 import { h, Component } from 'preact';
 
+import HeaderNav from '../shared/header-nav/header-nav.js';
+
 import style from './media-view.scss';
 
 export default class MediaView extends Component {
     render({ match }) {
         return (
             <div>
+                <HeaderNav />
                 <h1>Media standalone view {match.params.id}</h1>
             </div>
         );
