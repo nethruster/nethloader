daeb3fb8bea86ec38196cf2d2b9faa5b0e280e93
Guillem Arias <gariasfa7@alumnes.ub.edu>
2017-10-03 23:57:34 +0200

    Improve media-view view
    Clean and refactor css

--- client/src/app/views/media-view/media-view.js
+++ client/src/app/views/media-view/media-view.js
@@ -6,16 +6,17 @@ import style from './media-view.scss';
 
 export default class MediaView extends Component {
     render({ match }) {
+        const testUrl = "";
         return (
             <div class={`${ style.mediaView } flex flex-full-center`}>
                 <div class={`${ style.mediaViewWrapper } flex flex-dc flex-full-center`}>
                     <div class={ style.mediaViewImage }>
-                        <img src="" />
+                        <img src={testUrl} />
                     </div>
                     <div class={`${ style.mediaViewInfo } flex flex-dc`}>
                         <div class={`${ style.mediaViewButtons } flex flex-sa`}>
-                            <Button text="View full size" small icon="fullscreen" />
-                            <Button text="Download" small icon="download" />
+                            <a href={testUrl} target="_blank" rel="noopener"><Button text="View original" small icon="fullscreen" /></a>
+                            <a href={testUrl} download rel="noopener"><Button text="Download" small icon="download" /></a>
                         </div>
                         <p>Image info</p>
                     </div>
