2e71ffcb6e971be003c271ef8936b7c3367dc0cc
Claudio4 <me@claudio4.com>
2017-10-07 21:55:20 +0100

    Protec querys by authorization

--- server/src/graphql/resolvers/getters/query.js
+++
@@ -1,8 +0,0 @@
-const db = require('../../../models');
-
-module.exports = {
-  users: (parent, args) => db.User.findAll({ where: args }),
-  user: (parent, args) => db.User.findOne({ where: args }),
-  images: (parent, args) => db.Image.findAll({ where: args }),
-  image: (parent, args) => db.Image.findOne({ where: args })
-}
